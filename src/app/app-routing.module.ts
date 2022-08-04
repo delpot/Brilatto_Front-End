e42ee8535c6972ea328f75999a78eb9fdf9fe5b6
Nours06 <88280654+Nours06@users.noreply.github.com>
2022-08-04 10:11:57 +0200

    Add auth module and login and register routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { AllJewelCategoriesComponent } from './jewel-category/components/all-jewel-categories.component';
 import { JewelCategoryComponent } from './jewel-category/components/jewel-category.component';
 
 const routes: Routes = [
   { path: 'categories', component: AllJewelCategoriesComponent },
   { path: 'categories/:categoryName', component: JewelCategoryComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
