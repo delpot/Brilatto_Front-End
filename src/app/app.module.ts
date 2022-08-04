7c721f7776b54e227e37b85b6ff399581d39b803
Nours06 <88280654+Nours06@users.noreply.github.com>
2022-08-04 09:48:36 +0200

    Add banner.
    Add header component with menu, title and cart links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { JewelCategoryModule } from './jewel-category/jewel-category.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, JewelCategoryModule],
   bootstrap: [AppComponent],
 })
