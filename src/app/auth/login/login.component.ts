7b9af28ce02c2f1adc55115ba66a2d83b45bc29e
Nours06 <88280654+Nours06@users.noreply.github.com>
2022-08-05 13:03:47 +0200

    Add loading button and passwords don't match div

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -13,19 +13,25 @@ export class LoginComponent {
     password: '',
   };
 
+  isLoading: boolean = false;
+
   constructor() {}
 
   submit() {
+    if (this.isLoading) return;
+    this.isLoading = true;
     const auth = getAuth();
     signInWithEmailAndPassword(auth, this.form.email, this.form.password)
       .then((userCredential) => {
         console.log(userCredential);
+        alert('Vous êtes connecté!');
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         alert('Ces identifiants sont incorrects.');
-      });
+      })
+      .finally(() => (this.isLoading = false));
     return this.form;
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -14,20 +14,32 @@ export class RegisterComponent {
     passwordToConfirm: '',
   };
 
+  passwordsMatched: boolean = true;
+
+  isLoading: boolean = false;
+
   constructor() {}
 
   submit() {
+    if (this.isLoading) return;
     if (this.form.password !== this.form.passwordToConfirm) {
+      this.passwordsMatched = false;
       return;
     }
+    this.isLoading = true;
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
       .then((userCredential) => {
         console.log(userCredential);
+        alert('Vous êtes inscrit!');
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+      })
+      .finally(() => {
+        this.isLoading = false;
+        this.passwordsMatched = true;
       });
   }
 }
