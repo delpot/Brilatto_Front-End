1aa4e81726a961081a2ea568747726e51b3203c9
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2022-08-30 12:02:49 +0200

    Add login validation form.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { LoginForm } from '../models/login-form.interface';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { AuthService } from '../auth.service';
 import { HttpResponse } from '@angular/common/http';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,9 +14,10 @@ export class LoginComponent {
   loginDto: LoginForm;
   loginForm: FormGroup;
   isLoading: boolean = false;
-  isLoggedIn = false;
-  isLoginFailed = false;
-  errorMessage = '';
+  missingEmail: boolean = false;
+  missingPassword: boolean = false;
+  userNotFound: boolean = false;
+  wrongPassword: boolean = false;
 
   constructor(
     private authService: AuthService,
@@ -27,31 +28,54 @@ export class LoginComponent {
         password: ''
       }
       this.loginForm = this.formBuilder.group({
-        email: [null, Validators.required],
-        password: [null, Validators.required]
+        email: '',
+        password: ''
       })
     }
+
+  shouldDisplayRequiredError(control: AbstractControl | null): boolean {
+    if (control) {
+      return control.hasError('required') && control.touched;
+    }
+    return false;
+  }
 
   onSubmit(): void {
     if (this.isLoading) return;
     this.isLoading = true;
 
-    this.loginDto = this.loginForm.value
+    this.wrongPassword = false;
+    this.userNotFound = false;
 
-    this.authService
-    .login(this.loginDto)
-    .subscribe((res: HttpResponse<any>) => {
-        console.log(res);
-      });
+    this.loginDto = this.loginForm.value
 
     const auth = getAuth();
     
     signInWithEmailAndPassword(auth, this.loginDto.email, this.loginDto.password)
       .then((userCredential) => {
         console.log(`User logged in Firebase: ${userCredential.user.email}`);
+        this.authService
+        .login(this.loginDto)
+        .subscribe((res: HttpResponse<any>) => {
+          console.log(res)
+          });
         alert('Vous êtes connecté(e)!');
       })
       .catch((error) => {
+        switch (error.code) {
+          case 'auth/missing-email':
+            this.missingEmail = true;
+            break;
+          case 'auth/internal-error':
+            this.missingPassword = true;
+            break;
+          case 'auth/user-not-found':
+            this.userNotFound = true;
+            break;
+          case 'auth/wrong-password':
+            this.wrongPassword = true;
+            break;
+        }
         console.log(`Error code: ${error.code}. Error message: ${error.message}.`)
       })
       .finally(() => this.isLoading = false);
