4eb5b002e8fc4c1ea2a69f39de86bc67a0e4171b
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2022-08-30 10:53:09 +0200

    Use Reactive Forms for login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { LoginForm } from '../models/login-form.interface';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { AuthService } from '../auth.service';
 import { HttpResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,31 +11,42 @@ import { HttpResponse } from '@angular/common/http';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  form: LoginForm = {
-    email: '',
-    password: '',
-  };
-
+  loginDto: LoginForm;
+  loginForm: FormGroup;
   isLoading: boolean = false;
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    ) {
+      this.loginDto = {
+        email: '',
+        password: ''
+      }
+      this.loginForm = this.formBuilder.group({
+        email: [null, Validators.required],
+        password: [null, Validators.required]
+      })
+    }
 
   onSubmit(): void {
     if (this.isLoading) return;
     this.isLoading = true;
 
+    this.loginDto = this.loginForm.value
+
     this.authService
-    .login(this.form)
+    .login(this.loginDto)
     .subscribe((res: HttpResponse<any>) => {
         console.log(res);
       });
 
     const auth = getAuth();
     
-    signInWithEmailAndPassword(auth, this.form.email, this.form.password)
+    signInWithEmailAndPassword(auth, this.loginDto.email, this.loginDto.password)
       .then((userCredential) => {
         console.log(`User logged in Firebase: ${userCredential.user.email}`);
         alert('Vous êtes connecté(e)!');
