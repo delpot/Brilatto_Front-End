9fa1198f7341b7f7a68deadf6570d80376728e60
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2022-09-05 17:37:00 +0200

    Add model deletion

--- src/app/jewel-model/components/model-details/model-details.component.ts
+++ src/app/jewel-model/components/model-details/model-details.component.ts
@@ -1,17 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JewelModel } from '../../jewel-model.interface';
+import { JewelModelService } from '../../jewel-model.service';
 
 @Component({
   selector: 'app-model-details',
   templateUrl: './model-details.component.html',
   styleUrls: ['./model-details.component.css']
 })
-export class ModelDetailsComponent implements OnInit {
+export class ModelDetailsComponent {
   @Input() model: JewelModel = {} as JewelModel;
+  @Input() models: JewelModel[] = [];
 
-  constructor() { }
+  constructor(
+    private modelService: JewelModelService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
-  ngOnInit(): void {
+  deleteModel(modelId: string) {
+    this.modelService.deleteOneModel(modelId).subscribe({
+      next: (res) => {
+        const newModelList = this.models.filter((model) => model._id !== res._id);
+        this.models = newModelList;
+        const categoryId = this.route.snapshot.paramMap.get('categoryId');
+        this.router
+        .navigate(['/' + categoryId])
+        .then(() => window.location.reload());
+      },
+      error: (err) => {
+        console.log(`${err.statusText}: ${err.error}`);
+      }
+    });
   }
 
 }
